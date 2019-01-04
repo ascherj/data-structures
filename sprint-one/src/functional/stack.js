fe37a4ee597ac82bb56d03a55b6d97345109764e
Jake Ascher <ascherj@gmail.com>
2019-01-04 18:57:11 -0500

    Implement a stack using functional instantiation

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var key = size.toString();
+    storage[key] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    var topKey = size - 1;
+    var value = storage[topKey];
+    delete storage[topKey];
+    size = size === 0 ? 0 : size - 1;
+    console.log(size, value);
+    return value;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;

63f15591e309b11bf6b139ce07c188d7290c7b98
Jake Ascher <ascherj@gmail.com>
2019-01-05 14:14:08 -0500

    Implement a queue using es6 instantiation

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,23 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+    this.lowest = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  dequeue() {
+    var value = this.storage[this.lowest];
+    delete this.storage[this.lowest];
+    this.lowest++;
+    return value;
+  }
+
+  size() {
+    return (this.count - this.lowest < 0) ? 0 : this.count - this.lowest;
+  }
 }

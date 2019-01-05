8cbf296577ee61f382acdae9d9340b3039d71e21
Jake Ascher <ascherj@gmail.com>
2019-01-05 11:11:05 -0500

    Implement a queue using prototypal instantiation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,25 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.count = 0;
+  someInstance.lowest = 0;
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+queueMethods.dequeue = function() {
+  var value = this.storage[this.lowest];
+  delete this.storage[this.lowest];
+  this.lowest++;
+  return value;
+};
+
+queueMethods.size = function() {
+  return (this.count - this.lowest < 0) ? 0 : this.count - this.lowest;
+};

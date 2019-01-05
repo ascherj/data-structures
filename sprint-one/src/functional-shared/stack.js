2015b05400ef6edbfca5f0f3398e1fafdd1780ef
Jake Ascher <ascherj@gmail.com>
2019-01-04 22:54:20 -0500

    Implement a stack using functional instantiation w/ shared methods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  extend(someInstance, stackMethods);
+  someInstance.storage = {};
+  someInstance.count = 0;
+  return someInstance;
 };
+
+var extend = function(obj1, obj2) {
+  for (var key in obj2) {
+    obj1[key] = obj2[key];
+  }
+}
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+stackMethods.pop = function() {
+  var topKey = this.count - 1;
+  var value = this.storage[topKey];
+  delete this.storage[topKey];
+  this.count = (this.count === 0) ? 0 : this.count - 1;
+  return value;
+};
+
+stackMethods.size = function() {
+  return this.count;
+};

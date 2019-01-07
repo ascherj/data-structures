608f520750752ff7394ec29ebaaa24eab3049e96
Jake Ascher <ascherj@gmail.com>
2019-01-07 18:19:55 -0500

    Implement Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+  if (index !== -1) {
+    this._storage.splice(index, 1);
+  }
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+  Complexity: What is the time complexity of the above functions?
+    add() - O(1)
+    contains() - O(n)
+    remove() - O(n)
  */

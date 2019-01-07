c824265d9e2d3691504f5f1d74b05ccc06acefab
Jake Ascher <ascherj@gmail.com>
2019-01-07 17:28:38 -0500

    Implement Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,59 @@
-
-
-// Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
 };
 
-// Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  if (!this.contains(node)) {
+    this.nodes[node] = { 'edges': {} };
+  }
 };
 
-// Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return !!this.nodes[node];
 };
 
-// Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  if (this.contains(node)) {
+    for (var edge in this.nodes[node].edges) {
+      delete this.nodes[edge].edges[node];
+    }
+    delete this.nodes[node];
+  }
 };
 
-// Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return !!this.nodes[fromNode].edges[toNode];
 };
 
-// Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  if (this.contains(fromNode) && this.contains(toNode)) {
+    this.nodes[fromNode].edges[toNode] = true;
+    this.nodes[toNode].edges[fromNode] = true;
+  }
 };
 
-// Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  if (this.hasEdge(fromNode, toNode)) {
+    delete this.nodes[fromNode].edges[toNode];
+    delete this.nodes[toNode].edges[fromNode];
+  }
 };
 
-// Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var node in this.nodes) {
+    cb(node);
+  }
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+  Complexity: What is the time complexity of the above functions?
+    addNode() - O(1)
+    contains() - O(1)
+    removeNode() - O(n)
+    hasEdge() - O(1)
+    addEdge() - O(1)
+    removeEdge() - O(1)
+    forEachNode() - O(n)
  */
 
 
